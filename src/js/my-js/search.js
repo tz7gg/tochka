dc1d16a1d0114f1e9a5fb67115f93a3925b9ad6c
tz7gg <69346690+tz7gg@users.noreply.github.com>
2022-11-09 01:40:38 +0500

    chot polomal...

--- src/js/my-js/search.js
+++ src/js/my-js/search.js
@@ -4,11 +4,14 @@ const exchangeError = document.querySelector('.exchanger__error')
 const balanceWrap = document.querySelector('.exchanger__balance')
 const options = document.querySelector('.exchanger__options')
 
+let userInfo
+
 window.addEventListener('load', () => {
     searchBtn.onclick = async() => {
         if (searchInput.value) {
             spinner(true)
             let data = await getUser(searchInput.value).then(res => res.json())
+            userInfo = data
             if (data.id === 0) {
                 exchangeError.innerText = 'Пользователь не найден!'
                 exchangeError.classList.remove('dn')
